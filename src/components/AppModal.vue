2cc0f47b973850b6a36081a5e87df1b3ce122e86
rakushka-san <vladislavmohov@mail.ru>
2023-09-20 21:39:24 +0300

    Блок модального окна

---
+++ src/components/AppModal.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="modal">
+		<slot></slot>
+	</div>
+</template>
+
+<style lang="scss">
+@import './../assets/scss/colors';
+
+.modal {
+	position: absolute;
+
+	border-left: 1px solid $colorPrimaryBorder;
+	background-color: rgba($colorSecondaryBackground, 0.9);
+	backdrop-filter: blur(8px);
+}
+</style>
