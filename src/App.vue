ad10c596a0c03c20653cb928cbd6c955f77c1532
rakushka-san <vladislavmohov@mail.ru>
2023-09-20 19:22:37 +0300

    Разметка бокового окна

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AppFrame from './components/AppFrame.vue'
+</script>
 
-<template></template>
+<template>
+	<main>
+		<div class="container">
+			<AppFrame class="frame" />
+		</div>
+	</main>
+</template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.container {
+	padding: 2rem;
+
+	display: grid;
+	grid-template-columns: 14.75rem 32.8125rem;
+	grid-template-rows: 31.25rem 4.5rem;
+	gap: 1.5rem;
+}
+
+.frame {
+	width: 100%;
+}
+</style>
---
+++ src/components/AppFrame.vue
@@ -0,0 +1,60 @@
+<template>
+	<div class="frame">
+		<img
+			src="./../assets/img/frame-img.png"
+			alt="Frame Image"
+			class="frame__image"
+		/>
+		<div class="frame__header"></div>
+		<div class="frame__line"></div>
+		<div class="frame__line"></div>
+		<div class="frame__line"></div>
+		<div class="frame__line"></div>
+		<div class="frame__line"></div>
+		<div class="frame__line"></div>
+	</div>
+</template>
+
+<style lang="scss">
+@import './../assets/scss/colors';
+
+.frame {
+	padding: 1.125rem 0.875rem 1.5rem;
+
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+
+	border-radius: 0.75rem;
+	border: 1px solid $colorPrimaryBorder;
+
+	background-color: $colorSecondaryBackground;
+
+	&__image {
+		width: 100%;
+		aspect-ratio: 13 / 15;
+		object-fit: cover;
+		border-radius: 0.5rem;
+	}
+
+	&__header {
+		margin-top: 1.25rem;
+
+		width: 100%;
+		height: 1.625rem;
+
+		background: $gradientSkeleton;
+		border-radius: 0.5rem;
+	}
+
+	&__line {
+		margin-top: 1rem;
+
+		width: 100%;
+		height: 0.625rem;
+
+		background: $gradientSkeleton;
+		border-radius: 0.25rem;
+	}
+}
+</style>
